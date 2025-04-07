c8351f528aec79f97080993737f61d147ae5f546
Joana Amorim <joanasouzacontato@gmail.com>
2025-04-07 18:36:20 -0300

    recuperar senha page

--- src/assets/components/pages/RecuperarSenhaPage.jsx
+++ src/assets/components/pages/RecuperarSenhaPage.jsx
@@ -14,16 +14,16 @@ export default function RecuperarSenhaPage() {
   
             <div className="space-y-4">
               <div className="space-y-2">
-                <label htmlFor="email">Email</label>
-                <input id="email" placeholder="seu@email.com" type="email" />
+                <label htmlFor="email" className="gap-1">Email:</label>
+                <input id="email" placeholder="seu@email.com" type="email" className="border-1 rounded-sm p-1 w-full" />
               </div>
   
-              <button className="w-full" onClick={()=>alert("Confira seu email")}>Enviar Link de Recuperação</button>
+              <button className="w-full cursor-pointer bg-black text-white rounded-sm p-1" onClick={()=>alert("Confira seu email")}>Enviar Link de Recuperação</button>
             </div>
   
             <div className="flex justify-between pt-4">
 
-                <button onClick={()=>navigate("/login")}>Voltar para Login</button>
+                <button onClick={()=>navigate("/login")} className="cursor-pointer bg-gray-200 text-black p-1 rounded-sm">Voltar para Login</button>
 
             </div>
           </div>
