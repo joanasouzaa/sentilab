8d8349ca19aefc9bf44e6435b2a4cde089af9ea6
Joana Amorim <joanasouzacontato@gmail.com>
2025-04-21 19:35:22 -0300

    modificações na página de escolher como logar

--- src/assets/components/pages/LoginPage.jsx
+++ src/assets/components/pages/LoginPage.jsx
@@ -1,16 +1,9 @@
 import { useNavigate } from "react-router-dom"
 import styles from "../../styles/Login/LoginPage.module.css"
-import { useState } from "react";
 
 export default function LoginPage() {
 
     const navigate = useNavigate();
-
-    const handleCadastro = async (e)=>{
-        e.preventDefault();
-
-        const response = await fetch('hht')
-    }
 
 
     return (
@@ -25,14 +18,14 @@ export default function LoginPage() {
 
                         <div className="space-y-4">
 
-                            <button className="border-[#D97C2B] w-full h-16 text-lg justify-start border-1 rounded-sm cursor-pointer" onClick={() => navigate("/login/professor")}>Entrar como Professor </button>
+                            <button className="border-[#D97C2B] w-full h-16 text-lg font-bold justify-start border-1 rounded-sm cursor-pointer hover:bg-[#D97C2B] hover:text-white focus:outline-offset-2" onClick={() => navigate("/login/professor")}>Entrar como Professor </button>
 
-                            <button className="border-[#D97C2B] w-full h-16 text-lg justify-start border-1 rounded-sm cursor-pointer" onClick={() => navigate("/login/aluno")}>Entrar como Aluno</button>
+                            <button className="border-[#D97C2B] w-full h-16 text-lg font-bold justify-start border-1 rounded-sm cursor-pointer hover:bg-[#D97C2B] hover:text-white" onClick={() => navigate("/login/aluno")}>Entrar como Aluno</button>
                         </div>
 
                         <div className="flex justify-between pt-4">
-                            <button className="cursor-pointer hover:scale-105" onClick={() => navigate("/")}>Voltar</button>
-                            <button className={`${styles.btnCadastrar} text-white font-bold p-2 rounded-sm cursor-pointer`} onClick={() => navigate("/cadastrar")}>Cadastrar</button>
+                            <button className="cursor-pointer hover:scale-105 hover:underline" onClick={() => navigate("/")}>Voltar</button>
+                            <button className="bg-[#D97C2B] text-white font-bold p-2 rounded-sm cursor-pointer" onClick={() => navigate("/cadastrar")}>Cadastrar</button>
                         </div>
                     </div>
                 </div>
